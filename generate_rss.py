3e5ca3b0102b7989d8efcce9948da39691d2e1eb
Nelson Chen <crazysim@gmail.com>
2015-07-22 01:13:53 -0700

    modify existing RSS feed

--- generate_rss.py
+++ generate_rss.py
@@ -1,12 +1,20 @@
-__author__ = 'nelson'
-
+import re
 import requests
-import xml.etree.ElementTree as etree
+import xml.etree.ElementTree as ETree
 
-request = requests.get('http://www.smbc-comics.com/rss.php', stream=True)
 
-tree = etree.parse(request.raw)
+def update_rss():
+    request = requests.get('http://www.smbc-comics.com/rss.php', stream=True)
+    tree = ETree.parse(request.raw)
+    root = tree.getroot()
 
-root = tree.getroot()
+    for description in root.findall('./channel/item/description'):
+        pattern = r'<img src="http://www\.smbc-comics\.com/comics/(\d+\-\d+)\.([pg][ni][gf])"/>(.*)'
+        replace = r'<img src="http://www\.smbc-comics\.com/comics/\1\.\2"><br><br>' \
+                  r'<img src="http://www\.smbc-comics\.com/comics/\1after\.png">\3'
+        description.text = re.sub(pattern, replace, description.text)
 
-root
+    new_feed = ETree.tostring(root)
+
+if __name__ == "__main__":
+    update_rss()
