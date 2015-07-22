b64bc895b48092d88b2449d853b7fdb6b88bb6f0
Nelson Chen <crazysim@gmail.com>
2015-07-22 01:23:37 -0700

    Hey, it works! I'll unhardcode this later and work on transistion docs

--- generate_rss.py
+++ generate_rss.py
@@ -1,7 +1,7 @@
+import boto3
 import re
 import requests
 import xml.etree.ElementTree as ETree
-
 
 def update_rss():
     request = requests.get('http://www.smbc-comics.com/rss.php', stream=True)
@@ -10,11 +10,17 @@ def update_rss():
 
     for description in root.findall('./channel/item/description'):
         pattern = r'<img src="http://www\.smbc-comics\.com/comics/(\d+\-\d+)\.([pg][ni][gf])"/>(.*)'
-        replace = r'<img src="http://www\.smbc-comics\.com/comics/\1\.\2"><br><br>' \
-                  r'<img src="http://www\.smbc-comics\.com/comics/\1after\.png">\3'
+        replace = r'<img src="http://www.smbc-comics.com/comics/\1.\2"><br><br>' \
+                  r'<img src="http://www.smbc-comics.com/comics/\1after.png">\3'
         description.text = re.sub(pattern, replace, description.text)
 
-    new_feed = ETree.tostring(root)
+    processed_feed = ETree.tostring(root)
+
+    s3 = boto3.resource('s3')
+    s3.Bucket('smbc-rss-plus.mindflakes.com').put_object(Key='rss.xml',
+                                                         Body=processed_feed,
+                                                         ACL='public-read',
+                                                         ContentType='application/xml')
 
 if __name__ == "__main__":
     update_rss()
